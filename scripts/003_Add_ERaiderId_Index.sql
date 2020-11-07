feee77b65db328cd16abdbcaa3d3bf97e72ed4dd
Scott <millerscottdavid@outlook.com>
2020-11-06 21:07:35 -0500

    remove old .sql scripts from Rob

--- scripts/003_Add_ERaiderId_Index.sql
+++
@@ -1,5 +0,0 @@
-
-CREATE UNIQUE NONCLUSTERED INDEX IX_UWCUsers_eRaiderId 
-    ON dbo.UWCUsers (eraiderId) 
-    WHERE eRaiderId IS NOT NULL;
-GO
--- scripts/005_Roles.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO [dbo].[Roles] ([RoleName] ,[Description])
-VALUES
-('Administrator', 'Oversees entire system'),
-('Client', 'Submit tutorials to the UWC'),
-('Online Tutor', 'Tutors for the Online Writing Center'),
-('Onsite Tutor', 'Tutors onsite'),
-('Student Assistant', 'Enter onsite tutorial data')
